d80edb2e01e416998b435446b68992bb626ecf2c
Narek <Narek90@yandex.ru>
2021-11-09 12:52:36 +0400

    change names

--- src/App/View/index.js
+++ src/App/View/index.js
@@ -1,9 +1,9 @@
-import AboutUs from "./AboutUs/AboutUs";
-import Boxes from "./Boxes/Boxes";
-import ContactUs from "./ContactUs/ContactUs";
-import HomePage from './HomePage/HomePage'
-import Login from './Login/Login'
-import Register from './Register/Register'
-import Services from './Services/Services'
+import AboutUs from "./AboutUs";
+import Boxes from "./Boxes";
+import ContactUs from "./ContactUs";
+import HomePage from './HomePage'
+import Login from './Login'
+import Register from './Register/index'
+import Services from './Services'
 
  export {AboutUs,Boxes,ContactUs,HomePage,Login,Register,Services}
