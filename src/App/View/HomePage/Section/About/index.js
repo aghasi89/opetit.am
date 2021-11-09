f2427656ed4ec05ce61413a0865563ec16e6f581
Narek <narek90@yandex.ru>
2021-11-10 01:49:42 +0400

    Add about us

--- src/App/View/HomePage/Section/About/index.js
+++ src/App/View/HomePage/Section/About/index.js
@@ -1,12 +1,22 @@
 import './About.css'
-export default ()=>{
-return (
-    <div className='aboutUsGeneralConteiner'>
-        <div className="aboutUsTitle"><h1>About us</h1></div>
-        <div className="aboutUsbody"><span>Lorem ipsum dolor sit, amet consectetur 
-            adipisicing elit. Incidunt vero cum non excepturi nulla iste nam debitis a 
-            nemo, error nobis itaque tenetur repudiandae blanditiis reprehenderit amet 
-            ea voluptate recusandae?</span></div>
-    </div>
-)
+export default () => {
+    return (
+        <div className='aboutUsGeneralConteiner'>
+            <div className="aboutUsTitle"><h1>About us</h1></div>
+            <div className="aboutUsbody">
+                <span>
+                    Lorem ipsum dolor sit, amet consectetur
+                    adipisicing elit. Incidunt vero cum non excepturi nulla iste nam debitis a
+                    nemo, error nobis itaque tenetur repudiandae blanditiis reprehenderit amet
+                    ea voluptate recusandae?Lorem ipsum dolor sit, amet consectetur
+                    adipisicing elit. Incidunt vero cum non excepturi nulla iste nam debitis a
+                    nemo, error nobis itaque tenetur repudiandae blanditiis reprehenderit amet
+                    ea voluptate recusandae?Lorem ipsum dolor sit, amet consectetur
+                    adipisicing elit. Incidunt vero cum non excepturi nulla iste nam debitis a
+                    nemo, error nobis itaque tenetur repudiandae blanditiis reprehenderit amet
+                    ea voluptate recusandae?
+                </span>
+            </div>
+        </div>
+    )
 }
