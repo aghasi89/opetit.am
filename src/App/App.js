9a226c566c77c0f9a10e3b7a1ea76dc8d5d22391
ohanyanmarine <m.v.ohanyan@gmail.com>
2021-11-09 13:00:20 +0400

    replaced files to App, created Input component

--- src/App/App.js
+++ src/App/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Input from './components/Input';
 
 function App() {
   return (
     <div className="App">
-      Hello
+      <Input/>
     </div>
   );
 }
---
+++ src/App/components/Input/Input.js
@@ -0,0 +1,9 @@
+import "./input.css"
+
+export default function Input () {
+    return (
+        <div>
+            <input type="text" placeholder="Username" className="username"/>
+        </div>
+    )
+}
