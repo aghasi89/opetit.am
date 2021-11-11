fc1c6b39267a37db26536d32cbb027a7aca086fa
JenyaPetrosan <petrosyan.jenya@bk.ru>
2021-11-11 09:53:06 +0400

    Emilia changes

---
+++ src/Boxes/Header/HeaderSpan.js
@@ -0,0 +1,10 @@
+export default function HeaderSpan(props) {
+    return (
+      <div className="headerSpan">
+        <span>
+          {props.span}
+        </span>
+      </div>
+    );
+  }
+  
---
+++ src/Boxes/Img/ImgSrc.js
@@ -0,0 +1,8 @@
+export default function ImgSrc(props) {
+    return (
+      <div className="imgSrc">
+        <img src={props.imgsrc} />
+      </div>
+    );
+  }
+  
