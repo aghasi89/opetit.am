5eb508f2e4b89f327c6fc3611d6f9fbaaceaa730
Tatev Haroyan <tatevharoyan98@gmail.com>
2021-11-15 12:26:31 +0400

    conflict

--- src/App/App.js
+++ src/App/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 // import Input from './components/Input';
-import Header from './components/Header';
+import Header from './Components/Header';
 import HeaderSlide from './view/Home/Section/HeaderSlide';
 
 function App() {
   return (
     <div className="App">
-        <Header/>
-        <HeaderSlide/>
-        
+      <Header />
+      <HeaderSlide />
+
     </div>
   );
 }
