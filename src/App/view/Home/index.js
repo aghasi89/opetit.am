db00138c4304aa62c6beda4dcd1ebf036a78c444
Anushavan <anushavan777@gmail.com>
2021-12-08 20:33:26 +0400

    add sliderHooks

--- src/App/view/Home/index.js
+++ src/App/view/Home/index.js
@@ -1,11 +1,13 @@
 import HeaderSlide from './Section/HeaderSlide';
 import About from '../HomePage/Section/About';
+import { Boxes } from '..';
 
 export default () => {
     return (
         <div className="container">
             <HeaderSlide />
             <About />
+            <Boxes />
         </div>
     );
 };
